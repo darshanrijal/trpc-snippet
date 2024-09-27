81b499f4c56d8695c11e1dfb4a22881190751ac5
Darshan rijal <darshanrijal0@gmail.com>
2024-09-27 12:11:01 +0545

    hydrate

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import { TRPCProvider } from "@/trpc/TRPCProvider";
+import { HydrateClient } from "@/trpc/server";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -29,7 +30,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <TRPCProvider>{children}</TRPCProvider>
+        <TRPCProvider>
+          <HydrateClient>{children}</HydrateClient>
+        </TRPCProvider>
       </body>
     </html>
   );
